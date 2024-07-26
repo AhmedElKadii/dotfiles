20cb185c17ad3291c24f60f7ec299c389c0bd9d8
Ahmed El Kadii <ahmedelkadii@flowengine.app>
2024-07-26 07:55:14 +0300

    init

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- Show Non-Relative Numbers
+vim.cmd("set number")
+vim.cmd("set norelativenumber")
+vim.cmd("set nocursorline")
